dd94356ae1f61e4e6d564422c0bf9c1ad00cc5cb
qayol1 <qayol@freemail.hu>
2017-06-07 21:27:33 +0200

    both clone function implemented

--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -8,15 +8,40 @@ namespace CreateClass
 {
     class Person
     {
-        protected enum Genders
+        public enum Genders
         {
             Male,
             Female
         };
 
-        protected String name;
-        protected DateTime birthDate;
-        protected Genders gender;
+        private String name;
+        private DateTime birthDate;
+        private Genders gender;
+
+        public String Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public DateTime BirthDate
+        {
+            get => birthDate;
+            set => birthDate = value;
+        }
+
+        public Genders Gender
+        {
+            get => gender;
+            set => gender = value;
+        }
+
+        protected Person(String name, DateTime time, Genders gender)
+        {
+            this.name = name;
+            birthDate = time;
+            this.gender = gender;
+        }
 
         public override string ToString()
         {
@@ -26,38 +51,63 @@ namespace CreateClass
 
     }
 
-    class Employee : Person
+    class Employee : Person, ICloneable
     {
         private int salary;
         private String profession;
         private Room room;
 
+        public Room Room
+        {
+            get => room;
+            set => room = value;
+        }
+
+        Employee(String name, DateTime time, Genders gender,int salary, String profession) : base (name, time, gender)
+        {
+            this.salary = salary;
+            this.profession = profession;
+        }
+
         public override string ToString()
         {
-            String result = base.ToString() + ", Salary:" + salary + "$ ," + " Profession:" + profession + ", Room number:" + room.roomNumber;
+            String result = base.ToString() + ", Salary:" + salary + "$," + " Profession:" + profession + ", Room number:" + room.RoomNumber;
             return result;
         }
 
+        public object Clone()
+        {
+            Employee newEmployee = (Employee)this.MemberwiseClone();
+            newEmployee.Room = new Room(Room.RoomNumber);
+            return newEmployee;
+        }
+
+        /*public object Clone()
+        {
+            return this.MemberwiseClone();
+        }*/
+
         static void Main(string[] args)
         {
-            Employee person = new Employee()
-            {
-                name = "Kiss János",
-                birthDate = new DateTime(1974, 2, 9),
-                gender = Genders.Male,
-                salary = 9000,
-                profession = "Programmer"
-            };
-            Room room = new Room(1);
-            person.room = room;
-            Console.WriteLine(person.ToString());
+            Employee Kovacs = new Employee("Kovacs János", new DateTime(1974, 2, 9), Genders.Male, 9000, "Programmer");
+            Kovacs.Room = new Room(111);
+            Employee Kovacs2 = (Employee)Kovacs.Clone();
+            Kovacs2.Room.RoomNumber = 112;
+            Console.WriteLine(Kovacs.ToString());
+            Console.WriteLine(Kovacs2.ToString());
             Console.ReadKey();
         }
     }
 
     class Room
     {
-        public int roomNumber;
+        private int roomNumber;
+
+        public int RoomNumber
+        {
+            get => roomNumber;
+            set => roomNumber = value;
+        }
 
         public Room(int number)
         {
